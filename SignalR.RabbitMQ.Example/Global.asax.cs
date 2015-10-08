387ca5b1575c568173ed4baa7c22842aeec9e37f
Chris Robison <cdrobison@xactware.com>
2015-10-08 15:23:30 -0600

    Updating example to use mongo rabbit library

--- SignalR.RabbitMQ.Example/Global.asax.cs
+++ SignalR.RabbitMQ.Example/Global.asax.cs
@@ -1,25 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using Microsoft.AspNet.SignalR;
 using RabbitMQ.Client;
+using SignalR.MongoRabbit;
 
 namespace SignalR.RabbitMQ.Example
 {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            
+
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
@@ -32,9 +30,11 @@ namespace SignalR.RabbitMQ.Example
                 HostName = "localhost"
             };
 
+            var mongoConnectionString = "mongodb://localhost/signalr-test";
             var exchangeName = "SignalR.RabbitMQ-Example";
-            var configuration = new RabbitMqScaleoutConfiguration(factory, exchangeName);
-            GlobalHost.DependencyResolver.UseRabbitMq(configuration);
+            var configuration = new MongoRabbitScaleoutConfiguration(mongoConnectionString, factory, exchangeName);
+            var connection = new MongoRabbitConnection(configuration);
+            GlobalHost.DependencyResolver.UseRabbitMqAdvanced(connection, configuration);
         }
     }
 }
